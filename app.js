53251e2f96e8e381ba3b5008c7661bff76b17211
Julio Chaverra <daniel_0909@outlook.com>
2025-02-04 21:02:06 -0500

    Implementacion de la funcion agregar amigos y la visualizacion de la lista de estos.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listado = [];
+
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById('amigo');
+  let nombreAmigoText = nombreAmigo.value
+  nombreAmigo.value = '';
+  listado.push(nombreAmigoText);
+  let listadoAmigos = document.getElementById('listaAmigos');
+  listadoAmigos.innerHTML = '';
+  for(let i = 0; i < listado.length; i++) {
+    listadoAmigos.innerHTML += '<li>' + listado[i] + '</li>';
+  }
+}
